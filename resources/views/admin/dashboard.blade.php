939e092ccf6e2a7c3f63a6287c24a7194174e7a5
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-02-26 20:43:17 +0200

    Added FB login & admin model

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.master')
+@section('title','Admin dashboard')
+
+@section('content')
+ <div class="container">
+     Admin dashboard <a href="#">Regisetr</a>
+     <a href="#">Logout</a>
+ </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('admin/login','Admin\Auth\LoginController@showAdminLoginForm')->name('show.admin.login');
+
+
